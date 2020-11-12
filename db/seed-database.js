60a5e092124909ed944c599c82c22bd0a97c6f11
Joshua Ding <joshding@github.com>
2020-11-12 12:08:18 -0800

    database schema

--- db/seed-database.js
+++ db/seed-database.js
@@ -5,13 +5,29 @@ mongoose.connect('mongodb://localhost/realestate', {useNewUrlParser: true}).catc
     console.log('error connecting! ', err);
   }
 });
+// need to know neighborhood of primary record
+//will need a property schema
+//property schema needs to be linked to the neighborhood
+// needs to be linked to 12 other properties in that neighborhood. How to link this property to 12 other properties? Those 12 other properties should be in neighborhood. Neighborhood/ city.
+// sometimes there is a listing agent ID.
+// image portion looks good, need to tie it to neighborhood (faker has city pictures)
+
+const propertySchema = new mongoose.Schema({
+  id: Number,
+  neighborhood: String,
+  relatedProperties: [{type: Schema.Types.ObjectId, ref: 'Property'}],
+  listingAgentID: String
+
+})
 
 const imageSchema = new mongoose.Schema({
   id: Number,
+  imageURL: String,
   Address: String,
   Beds: Number,
   Baths: Number,
-  Sqft: Number
+  Sqft: Number,
+  propertyId: {type: Schema.Types.ObjectId, ref: 'Property' }
 });
 
 const Image = mongoose.model('Image', imageSchema);
